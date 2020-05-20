a752fbd96d4f209376ea9b334bf3bcca4beb8f6d
git <t.maczka85@wp.pl>
2020-05-20 13:45:39 +0200

    added and modified "kontakt" component

--- src/components/view/Kontakt.js
+++ src/components/view/Kontakt.js
@@ -10,7 +10,7 @@ function Kontakt() {
     const [show, setShow] = useSpring(() => ({
         opacity: 0, transform: "translateY(50px)"}))
     const [button, setButton] = useSpring(() => ({
-        color: "red", backgroundColor: "white"}))
+        color: "red", backgroundColor: "white", border: "2px solid red"}))
     // input errors
     const [nameErr, setNameErr] = useState(" ")
     const [emailErr, setEmailErr] = useState(" ")
@@ -27,11 +27,11 @@ function Kontakt() {
     //for button
     const buttonOverHandler = () => {
         setButton(() => ({
-            color: "white", backgroundColor: "red"
+            color: "white", backgroundColor: "red", border: "2px solid red"
         }))}
     const buttonUnhoverHandler = () => {
         setButton(() => ({
-            color: "red", backgroundColor: "white"
+            color: "red", backgroundColor: "white", border: "2px solid red"
         }))
     }
 
@@ -39,35 +39,56 @@ function Kontakt() {
     const nameRef = useRef(null)
     const emailRef = useRef(null)
     const messageRef = useRef(null)
+    const [validationInfo, setValidationInfo] = useState(" ")
     const submitHandler = (e) => {
+        const validationCheck = [false, false,false]
         e.preventDefault()
         nameRef.current = e.target.name.value
         emailRef.current = e.target.email.value
         messageRef.current = e.target.message.value
     // name validation
         if(nameRef.current.length < 3) {
+            validationCheck[0] = false
             setNameErr('*Pole "Imię" musi posiadać więcej niż 3 znaki.')
             setNameErrAnim(() => ({transform: "translateX(0px)", opacity: 1}))
         } else {
+            validationCheck[0] = true
             setNameErr(" ")
             setNameErrAnim(() => ({transform: "translateX(-20px)", opacity: 0}))
         }
     // email validation
         let emailValid = emailRef.current.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
         if(emailValid) {
+            validationCheck[1] = true
             setEmailErr(" ")
             setEmailErrAnim(() => ({transform: "translateX(-20px)", opacity: 0}))
         } else {
+            validationCheck[1] = false
             setEmailErr('*Proszę podać prawidłowy email.')
             setEmailErrAnim(() => ({transform: "translateX(0px)", opacity: 1}))
         }
     //message validation
-        if(emailRef.current.length < 8) {
+        if(messageRef.current.length < 8) {
+            validationCheck[2] = false
             setMessageErr('*Pole "Wiadomość" musi zawierać więcej niż 8 znaków.')
             setMessageErrAnim(() => ({transform: "translateX(0px)", opacity: 1}))
         } else {
+            validationCheck[2] = true
             setMessageErr(" ")
             setMessageErrAnim(() => ({transform: "translateX(-20px)", opacity: 0}))
+        }
+    // all inputs validated
+        if(validationCheck[0]===true && validationCheck[1]===true && validationCheck[2]===true) {
+            setValidationInfo("Wiadomość wysłana")
+            e.target.button.disabled = true
+            e.target.button.style.pointerEvents = "none"
+            e.target.name.disabled = true
+            e.target.email.disabled = true
+            e.target.message.disabled = true
+            setButton(() => ({
+                color: "lightgray", backgroundColor: "white", border: "2px solid lightgray"
+            }))
+            // here goes logic for sending email
         }
     }
 
@@ -84,10 +105,12 @@ function Kontakt() {
                     <NameInput nameErr={nameErr} errAnim={nameErrAnim} />
                     <EmailInput emailErr={emailErr} errAnim={emailErrAnim} />
                     <MessageInput messageErr={messageErr} errAnim={messageErrAnim} />
+                    <span className="validation-info">{validationInfo}</span>
                     <animated.button 
                         style={button}
                         onMouseOver={buttonOverHandler} 
                         onMouseLeave={buttonUnhoverHandler}
+                        name="button"
                         type="submit">Wyślij</animated.button>
                 </form>
             </animated.div>
